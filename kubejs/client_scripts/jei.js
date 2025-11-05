a74ae786c8a2ebaa776ca7f211f3d138a4d67658
Elle <115524533+hexagonelle@users.noreply.github.com>
2025-11-04 17:26:58 -0800

    Script adjustment (in progress)

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -1,8 +1,67 @@
 JEIEvents.information(event => {
-  event.addItem(
-  	Item.of(
-  		'minecraft:enchanted_book',
-  		'{StoredEnchantments:[{id:"minecraft:efficiency",lvl:5s}]}'
-	),
-  	['Obtained by trading with', 'a Desert Librarian villager'])
+
+	const desertEnchants = [
+		"Fire Protection",
+		"Thorns",
+		"Infinity",
+		"Efficiency"
+	];
+	const plainsEnchants = [
+		"Bane of Arthropods",
+		"Smite",
+		"Punch",
+		"Protection"
+	];
+	const savannaEnchants = [
+		"Knockback",
+		"Curse of Binding",
+		"Sweeping Edge",
+		"Sharpness"
+	];
+	const snowEnchants = [
+		"Looting",
+		"Aqua Affinity",
+		"Frost Walker",
+		"Silk Touch"
+	]
+	const taigaEnchants = [
+		"Blast protection",
+		"Fire aspect",
+		"Flame",
+		"Fortune"
+	]
+	const jungleEnchants = [
+		"Projectile Protection",
+		"Power",
+		"Feather Falling",
+		"Unbreaking"
+	]
+	const swampEnchants = [
+		"Respiration",
+		"Depth Strider",
+		"Curse of Vanishing",
+		"Mending"
+	]
+
+	const enchantsList = [
+		["Desert", desertEnchants],
+		["Plains", plainsEnchants],
+		["Savanna", savannaEnchants],
+		["Snow", snowEnchants],
+		["Taiga", taigaEnchants],
+		["Jungle", jungleEnchants],
+		["Swamp", swampEnchants]
+	]
+
+
+	
+
+	event.addItem(
+		Item.of('minecraft:enchanted_book'),
+		['Obtained by trading with a' + ' Librarian villager:'],
+		['']
+	)
+
+
+
 })
