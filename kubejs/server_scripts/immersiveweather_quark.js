a07c96bd25c3d72d10a09e57967ea919882f9b0b
Elle <115524533+hexagonelle@users.noreply.github.com>
2025-11-13 23:52:12 -0800

    Fixed typo in script

--- kubejs/server_scripts/immersiveweather_quark.js
+++ kubejs/server_scripts/immersiveweather_quark.js
@@ -3,7 +3,7 @@ ServerEvents.recipes(event => {
 	//////// IMMERSIVE WEATHERING x QUARK /////////
 
 	// Allow crafting between different permafrosts:
-	event.shapeless("immersive_weathering:limestone",["quark:limestone"])
-	event.shapeless("quark:limestone",["immersive_weathering:limestone"])
+	event.shapeless("immersive_weathering:permafrost",["quark:permafrost"])
+	event.shapeless("quark:permafrost",["immersive_weathering:permafrost"])
 
 })
