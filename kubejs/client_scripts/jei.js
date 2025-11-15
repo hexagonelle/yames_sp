5eddd96ab8fdcd6af64feedee64ab86c3318bc5e
Elle <115524533+hexagonelle@users.noreply.github.com>
2025-11-15 14:46:56 -0800

    Added jei script that gives info on crops/soils

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -53,7 +53,7 @@ JEIEvents.information(event => {
 		["Swamp", swampEnchants]
 	]
 
-	function jeiEntry(location, enchants, inputEvent){
+	function jeiEnchantEntry(location, enchants, inputEvent){
 		inputEvent.addItem(
 			'kubejs:enchanted_book',
 			[
@@ -74,9 +74,193 @@ JEIEvents.information(event => {
 		location = enchantSet[0];
 		enchants = enchantSet[1];
 
-		jeiEntry(location,enchants,event)
+		jeiEnchantEntry(location,enchants,event)
 
 	});
+
+
+
+	const earthenClayCrops = [
+		"Wheat",
+		"Bamboo"
+	];
+
+	const loamCrops = [
+		"Melons",
+		"Pumpkins",
+		"Sweet Berries",
+		"Tomatoes (currently defunct)"
+	];
+
+	const sandyDirtCrops = [
+		"Carrots",
+		"Beetroots",
+		"Cabbages"
+	];
+
+	const siltCrops = [
+		"Potato",
+		"Wheat",
+		"Cabbage",
+		"Onion",
+		"Supplementaries Flax"
+	];
+
+	const cropsList = [
+		["earthen_clay", earthenClayCrops],
+		["loam", loamCrops],
+		["sandy_dirt", sandyDirtCrops],
+		["silt", siltCrops]
+	]
+
+	function jeiCropEntry(soil, crops, inputEvent){
+
+		var cropListString = ""
+		var numcrops = crops.length;
+		for (var i = 0; i < numcrops; i++){
+			if(i == numcrops - 1){
+				cropListString = cropListString + crops[i]
+			} else {
+				cropListString = cropListString + crops[i] + ", "
+			}
+		}
+
+		console.log('soil: ' + soil);
+		console.log('cropListString: ' + cropListString);
+
+		inputEvent.addItem(
+			'immersive_weathering:' + soil,
+			[
+				'Can be used to grow the following crops:',
+				cropListString
+			]
+		)
+	}
+
+	var soil = "";
+	var crops = [];
+	cropsList.forEach(cropSet => {
+		soil = cropSet[0];
+		crops = cropSet[1];
+
+		jeiCropEntry(soil,crops,event)
+
+	});
+
+
+
+
+
+	const wheatSoil = [
+		"Earthen Clay",
+		"Silt",
+		"Rich Soil"
+	];
+
+	const bambooSoil = [
+		"Earthen Clay",
+		"Rich Soil"
+	];
+
+	const melonSoil = [
+		"Loam",
+		"Rich Soil"
+	];
+
+	const pumpkinSoil = [
+		"Loam",
+		"Rich Soil"
+	];
+
+	const sweetBerrySoil = [
+		"Loam",
+		"Rich Soil"
+	];
+
+	const tomatoSoil = [
+		"Loam",
+		"Rich Soil"
+	];
+
+	const carrotSoil = [
+		"Sandy Dirt",
+		"Rich Soil"
+	];
+
+	const beetrootSoil = [
+		"Sandy Dirt",
+		"Rich Soil"
+	];
+
+	const cabbageSoil = [
+		"Sandy Dirt",
+		"Silt"
+	];
+
+	const potatoSoil = [
+		"Silt",
+		"Rich Soil"
+	];
+
+	const onionSoil = [
+		"Silt",
+		"Rich Soil"
+	];
+
+	const flaxSoil = [
+		"Silt",
+		"Rich Soil"
+	];
+
+	const soilsList = [
+		["minecraft:wheat", wheatSoil],
+		["minecraft:bamboo", bambooSoil],
+		["minecraft:melon_slice", melonSoil],
+		["minecraft:pumpkin", pumpkinSoil],
+		["minecraft:sweet_berries", sweetBerrySoil],
+		["farmersdelight:tomato", tomatoSoil],
+		["minecraft:carrot", carrotSoil],
+		["minecraft:beetroot", beetrootSoil],
+		["farmersdelight:cabbage", cabbageSoil],
+		["minecraft:potato", potatoSoil],
+		["farmersdelight:onion", onionSoil],
+		["supplementaries:flax", flaxSoil]
+	]
+
+	function jeiSoilEntry(crop, soils, inputEvent){
+
+		var soilListString = ""
+		var numsoils = soils.length;
+		for (var i = 0; i < numsoils; i++){
+			if(i == numsoils - 1){
+				soilListString = soilListString + soils[i]
+			} else {
+				soilListString = soilListString + soils[i] + ", "
+			}
+		}
+
+		console.log('crop: ' + crop);
+		console.log('soilListString: ' + soilListString);
+
+		inputEvent.addItem(
+			crop,
+			[
+				'Grows faster on the following soils:',
+				soilListString
+			]
+		)
+	}
+
+	var crop = "";
+	var soils = [];
+	soilsList.forEach(soilSet => {
+		crop = soilSet[0];
+		soils = soilSet[1];
+
+		jeiSoilEntry(crop,soils,event)
+
+	});
+
 
 
 
